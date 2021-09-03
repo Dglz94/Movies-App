791b84398d114224eed5c14be2eeec3a7d35dcda
Dglz94 <dglz94@hotmail.com>
2021-09-03 15:35:31 -0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h2>Movie App</h2>
+      <div className="nav">
+        <a href="#">Inicio</a>
+        <a href="#">Programas</a>
+        <a href="#">Peliculas</a>
+        <a href="#">Más Recientes</a>
+        <a href="#">Mi lista</a>
+      </div>
+    </div>
+  );
+}
